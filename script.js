612d76c7ede1e6ed9017308111e49f4b21969383
Suzanne Tordjman <suzannetor@gmail.com>
2022-09-28 11:06:57 -0400

    position of the images

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ questions.forEach(question => {
   question.addEventListener("click", () => {
     if (question.nextElementSibling.classList.contains('hide')) {
       question.nextElementSibling.classList.remove('hide');
-      question.classList.add('bold')
+      question.classList.add('bold');
+      question.classList.remove('border-bottom');
     }
     else {
       question.nextElementSibling.classList.add('hide');
+      question.classList.add('border-bottom');
       question.classList.remove('bold')
     }
 
